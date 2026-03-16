37bbb58390439912294634da2d56142977078c12
s34043 <s34043@pjwstk.edu.pl>
2026-03-16 16:18:16 +0100

    Add Calculate Average method

--- apbd_task1/apbd_task1/Program.cs
+++ apbd_task1/apbd_task1/Program.cs
@@ -1,3 +1,20 @@
-﻿Console.WriteLine("sup, apbd");
+﻿int[] numbers = { 2, 4, 6, 8 };
+
+double average = CalculateAverage(numbers);
+Console.WriteLine("Average = " + average);
+
+Console.WriteLine("sup, apbd");
 string name = Console.ReadLine();
 Console.WriteLine("Hello " + name);
+
+static double CalculateAverage(int[] values)
+{
+    int sum = 0;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        sum += values[i];
+    }
+
+    return (double)sum / values.Length;
+}
